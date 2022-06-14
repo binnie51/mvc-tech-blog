3263c9902a76ea46923258c79c2e6916061c73ca
Vincent Tjia <vtjia51@gmail.com>
2022-06-14 17:46:58 -0400

    wrote C, R by id for blog routes. Changed route to direct users to /dashboard after successfully logged in on /public/js/login.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,2 +1,31 @@
 const router = require('express').Router();
+const { Blog, Comments } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+// CREATE new blog
+router.post('/', withAuth, async (res, req) => {
+    try {
+        const blogData = await Blog.create({
+            ...req.body,
+            title: req.body.title,
+            content: req.body.content,
+            author_id: req.session.user_id,
+        });
+        res.status(200).json(blogData);
+    }
+    catch (err) {
+        res.status(400).json({ message: "Error! Can't create blog entry."});
+    }
+});
+
+// GET/retrieves blog with specific id
+router.get('/:id', withAuth, async (req,res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id);
+        const blogObj = blogData.get({ plain: true });
+        res.status(200).json(blogObj);
+    }
+    catch (err) {
+        res.status(400).json({ message: "Error! The blog with that ID doesn't exist."})
+    }
+})
