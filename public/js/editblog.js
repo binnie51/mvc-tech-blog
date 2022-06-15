722fe0e469cfc9c55d83ac2f3e7b3b6a2d8103c3
Vincent Tjia <vtjia51@gmail.com>
2022-06-15 19:28:24 -0400

    wrote js/editBlog, signup

---
+++ public/js/editblog.js
@@ -0,0 +1,49 @@
+// UPDATE an entry
+const blodUpdateHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector('#project-funding').value.trim(); // need to define id in handlebars!!!!
+    const content = document.querySelector('#project-desc').value.trim(); // need to define id in handlebars!!!!
+  
+    if (title && content) {
+      const response = await fetch(req_url, {
+        method: 'POST',
+        body: JSON.stringify({ title, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to update this blog!');
+      }
+    }
+};
+
+// DELETE an entry
+const blogDeleteHandler = async (event) => {
+    event.preventDefault();
+  
+    const response = await fetch(req_url, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+    document.location.replace('/dashboard');
+    } else {
+    alert('Failed to delete blog entry!');
+    }
+};
+
+document
+  .querySelector('.blog-update')
+  .addEventListener('submit', blodUpdateHandler);
+
+document
+  .querySelector('.blog-delete')
+  .addEventListener('click', blogDeleteHandler);
