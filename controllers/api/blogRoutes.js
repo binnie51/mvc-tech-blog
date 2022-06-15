0cc7f6f9e653b494171434165a0022f4935d9f98
Vincent Tjia <vtjia51@gmail.com>
2022-06-15 18:51:05 -0400

    wrote CRUD for blogRoute

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 const { Blog, Comments } = require('../../models');
-const withAuth = require('../../utils/auth');
+const withAuthentication = require('../../utils/auth');
+
+// GET blog with specific id (only after users have signed in)
+router.get('/:id', withAuthentication, async (req,res) => {
+    try {
+        const blogData = await Blog.findByPk(req.params.id);
+        const blog = blogData.get({ plain: true });
+        res.status(200).json(blog);
+    }
+    catch (err) {
+        res.status(400).json({ message: "Error! The blog with that ID doesn't exist."})
+    }
+});
 
 // CREATE new blog
-router.post('/', withAuth, async (res, req) => {
+router.post('/', withAuthentication, async (res, req) => {
     try {
         const blogData = await Blog.create({
             ...req.body,
@@ -18,19 +30,67 @@ router.post('/', withAuth, async (res, req) => {
     }
 });
 
-// GET/retrieves blog with specific id
-router.get('/:id', withAuth, async (req,res) => {
+// UPDATE specific entry that belongs to the a user who has a matching 
+// username to the author of the blog post
+router.put('/:id', withAuthentication, async (req, res) => {
     try {
-        const blogData = await Blog.findByPk(req.params.id);
-        const blogObj = blogData.get({ plain: true });
-        res.status(200).json(blogObj);
+        if (req.body && req.params.id) {
+            const blogData = await Blog.update(req.body, {
+                where: {
+                    id: req.params.id,
+                    author_id: req.session.user_id,
+                },
+            });
+
+            if (!blogData) {
+                res.status(404).json({ message: 'No blog post found with this id!' });
+            }
+        }
+        res.status(200).json(blogData);
     }
     catch (err) {
-        res.status(400).json({ message: "Error! The blog with that ID doesn't exist."})
+        res.status(500).json(err);
     }
 });
 
-// CREATE comment 
-router.post('/:id/comments', withAuth, async (req, res) => {
-    
+// DELETE a specific blog entry that belongs to the a user who has a matching 
+// username to the author of the blog post
+router.delete('/:id', withAuthentication, async (res, req) => {
+    try {
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                author_id: req.params.user_id,
+            },
+        });
+
+        if (!blogData) {
+            res.status(404).json({ message: 'No blog post found with this id!' });
+        }
+        res.status(200).json(blogData);
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
+
+// Add comment to a specific blog post 
+router.post('/:id/comment', withAuthentication, async (req, res) => {
+    try {
+        if (req.body.content) {
+            const commentsData = await Comments.create({
+                content: req.body.content,
+                blog_id: req.body.blog_id,
+                commentor_id: req.session.user_id,
+            });
+            res.status(200).json(commentsData);
+        }
+        else {
+            res.status(404).json({ message: "Error! Can't comment." });
+        }
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
